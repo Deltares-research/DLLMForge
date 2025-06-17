3f05b9a4a361207e9f8e0788ecaa0d9093122361
JingDeng92 <jing.deng@deltares.nl>
2025-06-17 14:35:50 +0200

    Clarify input and output format requirements in document preprocessing script

--- dllmforge/rag_preprocess_documents.py
+++ dllmforge/rag_preprocess_documents.py
@@ -5,6 +5,7 @@ This script is used to preprocess the documents for the RAG pipeline.
 # General document loader
 # Based on: langchain_community.document_loaders (https://python.langchain.com/docs/integrations/document_loaders/)
 # select the right loader based on the file type. Here we provide examples on the most common file types: pdf, docx, txt, csv, json, html, markdown, etc.
+# the most important point is to know the format of input and output
 
 
 
