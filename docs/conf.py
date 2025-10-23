472a59a93e8383ec5988cd98b1cfa86f00842774
smyrniou <eleni.smyrniou@deltares.nl>
2025-10-23 13:13:57 +0200

    updated the config doc file

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,11 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('..'))  # Add this line
+
 import dllmforge
 
 project = 'dllmforge'
